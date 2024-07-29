747387090d0f6ce80a7ca44023bd8eb76c3eadab
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-07-29 15:19:06 -0400

    added class names to components

--- src/button.jsx
+++ src/button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Button({ label }) {
-  return <button>{label}</button>;
+  return <button className="button">{label}</button>;
 }
 
 export default Button;
--- src/header.jsx
+++ src/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header({ title }) {
   return (
-    <div>
+    <div className="header">
       <h1>{title}</h1>
     </div>
   );
